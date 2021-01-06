b2e431d6bc4e98cae3a0ea6d01c8a82c3462c84b
Bazulenkov <anton@bazul.ru>
2021-01-06 18:00:21 +0300

    Fix some bugs

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # path("ingredients/",),  # ? есть в js.api
-    path("favorites/", views.Favorites.as_view()),  # список избранных рецептов
+    path("favorites/", views.Favorites.as_view(), name="favorites"),  # список избранных рецептов
     path(
         "favorites/<int:recipe_id>/", views.Favorites.as_view()
     ),  # ? есть в js.api
--- foodgram/settings.py
+++ foodgram/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'recipes',
-    'api'
+    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
